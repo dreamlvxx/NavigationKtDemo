011f29cca2ad88d3eaac260f8a06a28dee39c0df
xingxinglv <lvxingxing@kingsoft.com>
2020-06-01 11:58:15 +0800

    init repo

---
+++ app/src/main/java/com/dream/navigationktdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dream.navigationktdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.main_host_fragment_nav).navigateUp()
+}
---
+++ app/src/main/java/com/dream/navigationktdemo/fragment/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.dream.navigationktdemo.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.dream.navigationktdemo.R
+import kotlinx.android.synthetic.main.fragment_first_layout.view.*
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root= inflater.inflate(R.layout.fragment_first_layout,container,false)
+        root.apply {
+            tv_first_btn.setOnClickListener {
+                Navigation.findNavController(it).navigate(R.id.action_first)
+            }
+        }
+        return root
+    }
+}
