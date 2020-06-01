af92d60f96d7c7e87a5f6ef8ed18bfcba1d96713
xingxinglv <lvxingxing@kingsoft.com>
2020-06-01 14:10:20 +0800

    add navigation node

--- app/src/main/java/com/dream/navigationktdemo/fragment/ForgetPasswordFragment.kt
+++ app/src/main/java/com/dream/navigationktdemo/fragment/ForgetPasswordFragment.kt
@@ -7,18 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.dream.navigationktdemo.R
-import kotlinx.android.synthetic.main.fragment_second_layout.view.*
+import kotlinx.android.synthetic.main.fragment_forget_pwd_layout.view.*
 
-class SecondFragment : Fragment(){
+class ForgetPasswordFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_second_layout,container,false)
+        val root = inflater.inflate(R.layout.fragment_forget_pwd_layout,container,false)
         root.apply {
-            tv_btn_topwd.setOnClickListener {
-                Navigation.findNavController(it).navigate(R.id.action_second)
+            tv_pwd_btn.setOnClickListener {
+                Navigation.findNavController(it).navigate(R.id.action_pwd)
             }
         }
         return root
